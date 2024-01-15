c516127e3cf466f9a9581506b3aa3052431dfef3
franli0 <yichun0701@gmail.com>
2024-01-15 17:37:32 +0800

    add more test cases

--- tests/solana-bootcamp-project.ts
+++ tests/solana-bootcamp-project.ts
@@ -2,48 +2,85 @@ import * as anchor from "@coral-xyz/anchor";
 import { SolanaBootcampProject } from "../target/types/solana_bootcamp_project";
 
 describe("solana-bootcamp-project", () => {
-  const ethToUSD = "EdVCmQ9FSPcVe5YySXDPCRmc8aDQLKJ9xvYBMZPie1Vw";
-  const usdtToUSD = "38xoQ4oeJCBrcVvca2cGk7iV1dAfrmTR1kmhSCJQ8Jto";
-
-  const provider = anchor.AnchorProvider.local();
+  const provider = anchor.AnchorProvider.env();
 
   // Configure the client to use the local cluster.
   anchor.setProvider(provider);
 
-  const config = anchor.web3.Keypair.generate();
   const program = anchor.workspace
     .SolanaBootcampProject as anchor.Program<SolanaBootcampProject>;
   const payer = provider.wallet.publicKey;
+  const btc_usd_price_feed = "HovQMDrbAgAYPCmHVSrezcSmkMtXSSUsLDFANExrZh2J";
+  const sol_usd_price_feed = "J83w4HKfqxwcq3BEMMkPFSppX3gqekLyLJBexebFVkix";
+  const eth_usd_price_feed = "EdVCmQ9FSPcVe5YySXDPCRmc8aDQLKJ9xvYBMZPie1Vw";
+  const inch_usd_price_feed = "2iRhuHfXDLmSqxSDo2Gyv59ZMaPDkensHHF2cJwWhaqW";
+  const aave_usd_price_feed = "FT7Cup6ZiFDF14uhFD3kYS3URMCf2RZ4iwfNEVUgndHW";
+  const bnb_usd_price_feed = "GwzBgrXb4PG59zjce24SF2b9JXbLEjJJTBkmytuEZj1b";
+  const cake_usd_price_feed = "5DcE5KDEN44w6gqqMvfBQXXD18nJ6Zm54o2j1BEtn24b";
+  const dai_usd_price_feed = "A8XFp1YSUqyDDvTwRXM1vmhPHCLxzncG45t1ZH9CMYq7";
 
-  it("Initialize the config.", async () => {
+  it("Fetch BTC Price", async () => {
     const tx = await program.methods
-      .init({
-        loanPriceFeedId: new anchor.web3.PublicKey(ethToUSD),
-        collateralPriceFeedId: new anchor.web3.PublicKey(usdtToUSD),
-      })
-      .accounts({
-        payer,
-        config: config.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-      })
-      .signers([config])
+      .fetchPrice("BTC")
+      .accounts({ signer: payer, priceFeed: btc_usd_price_feed })
       .rpc();
-
-    console.log("Config key: " + config.publicKey);
-    console.log("Init() is invoked: " + tx);
+    console.log("Your transaction signature", tx);
   });
 
-  it("Check loan to value ratio.", async () => {
+  it("Fetch SOL Price", async () => {
     const tx = await program.methods
-      .loanToValue(new anchor.BN(1), new anchor.BN(3000))
-      .accounts({
-        config: config.publicKey,
-        pythLoanAccount: new anchor.web3.PublicKey(ethToUSD),
-        pythCollateralAccount: new anchor.web3.PublicKey(usdtToUSD),
-      })
-      .signers([])
+      .fetchPrice("SOL")
+      .accounts({ signer: payer, priceFeed: sol_usd_price_feed })
       .rpc();
+    console.log("Your transaction signature", tx);
+  });
 
-    console.log("Loan2Value() is invoked: " + tx);
+  it("Fetch ETH Price", async () => {
+    const tx = await program.methods
+      .fetchPrice("ETH")
+      .accounts({ signer: payer, priceFeed: eth_usd_price_feed })
+      .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Fetch 1INCH Price", async () => {
+    const tx = await program.methods
+      .fetchPrice("1INCH")
+      .accounts({ signer: payer, priceFeed: inch_usd_price_feed })
+      .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Fetch AAVE Price", async () => {
+    const tx = await program.methods
+      .fetchPrice("AAVE")
+      .accounts({ signer: payer, priceFeed: aave_usd_price_feed })
+      .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Fetch BNB Price", async () => {
+    const tx = await program.methods
+      .fetchPrice("BNB")
+      .accounts({ signer: payer, priceFeed: bnb_usd_price_feed })
+      .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Fetch CAKE Price", async () => {
+    const tx = await program.methods
+      .fetchPrice("CAKE")
+      .accounts({ signer: payer, priceFeed: cake_usd_price_feed })
+      .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  // This should fail
+  it("Fetch DAI Price", async () => {
+    const tx = await program.methods
+      .fetchPrice("DAI")
+      .accounts({ signer: payer, priceFeed: dai_usd_price_feed })
+      .rpc();
+    console.log("This test should fail because DAI wasn't listed", tx);
   });
 });
